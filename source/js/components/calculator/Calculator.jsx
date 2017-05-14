c1fb046248d7a8cb65785b8a3dd67637637a492d
AdrianMindak <adrian.richard.mindak@gmail.com>
2017-05-14 21:51:36 +0200

    Zwischenstand
    einiges umstruckturiert
    muss noch alle functionien implementieren
    zahlen functionieren
    komma und clear functioniert

--- source/js/components/calculator/Calculator.jsx
+++ source/js/components/calculator/Calculator.jsx
@@ -7,19 +7,62 @@ export default class Calculator extends Component {
 	constructor(){
 		super();
 		this.state = {
-			viewportValue: '0'
+			viewportValue: '0',
+			hiddenCalculation: 0,
+			lastOperator: '+',
 		};
 	}
 
 	updateViewport(newValue) {
-		this.setState({ newValue });
+		switch (newValue) {
+			case "c":
+				this.setState({ viewportValue: "0", hiddenCalculation:"0", lastOperator: "+" });
+				break;
+			case "1":
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+			case "0":
+				this.setState({ viewportValue: this.state.viewportValue === '0' ? newValue : `${ this.state.viewportValue }${ newValue }` });
+				break;
+			case ".":
+				if (this.state.viewportValue.indexOf('.') === -1) {
+					this.setState({ viewportValue: `${ this.state.viewportValue }${ newValue }` });
+				}
+				break;
+				case "+":
+					switch (this.state.lastOperator) {
+						case "+":
+							this.setState({ viewportValue: this.state.hiddenCalculation + this.state.viewportValue.parseInt() });
+							break;
+						case "-":
+							this.setState({ viewportValue: this.state.hiddenCalculation - this.state.viewportValue });
+							break;
+						case "*":
+							this.setState({ viewportValue: this.state.hiddenCalculation * this.state.viewportValue });
+							break;
+						case "/":
+							this.setState({ viewportValue: this.state.viewportValue !== 0 ? this.state.hiddenCalculation / this.state.viewportValue : 'LOGICAL ERROR' });
+							break;
+						default:
+					}
+					this.setState({ lastOperator: '+' });
+					break;
+			default:
+
+		}
 	}
 
 	render() {
 		return (
 			<div className="calculatorBody">
 				<Viewport viewportValue={this.state.viewportValue} />
-				<Keyboard onClick={ this.updateViewport.bind(this) }/>
+				<Keyboard updateViewport={ this.updateViewport.bind(this) }/>
 			</div>
     );
   }
--- source/js/components/keyboard/Keyboard.jsx
+++ source/js/components/keyboard/Keyboard.jsx
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
 
-import Key from '../key/Key';
-
 export default class Keyboard extends Component {
+	updateViewport(e) {
+		const newValue = e.target.innerHTML;
+		this.props.updateViewport(newValue);
+	}
 	render() {
 		return (
 			<div className="keyboard">
 				<div className="keyboard-row">
-					<Key kind="operator" character="C" />
-					<Key kind="operator" character="SV" />
-					<Key kind="operator" character="%" />
-					<Key kind="operator" character="/" />
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>c</button>
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>+/-</button>
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>%</button>
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>/</button>
 				</div>
 				<div className="keyboard-row">
-					<Key kind="number" character="9" />
-					<Key kind="number" character="8" />
-					<Key kind="number" character="7" />
-					<Key kind="operator" character="*" />
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>9</button>
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>8</button>
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>7</button>
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>*</button>
 				</div>
 				<div className="keyboard-row">
-					<Key kind="number" character="6" />
-					<Key kind="number" character="5" />
-					<Key kind="number" character="4" />
-					<Key kind="operator" character="-" />
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>6</button>
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>5</button>
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>4</button>
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>-</button>
 				</div>
 				<div className="keyboard-row">
-					<Key kind="number" character="3" />
-					<Key kind="number" character="2" />
-					<Key kind="number" character="1" />
-					<Key kind="operator" character="+" />
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>3</button>
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>2</button>
+					<button className="number"  onClick={ this.updateViewport.bind(this) }>1</button>
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>+</button>
 				</div>
 				<div className="keyboard-row">
-					<Key kind="number" character="0" size="sizeH2" />
-					<Key kind="operator" character="." />
-					<Key kind="operator" character="=" />
+					<button className="number sizeH2"  size="2" onClick={ this.updateViewport.bind(this) }>0</button>
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>.</button>
+					<button className="operator" onClick={ this.updateViewport.bind(this) }>=</button>
 				</div>
 			</div>
     );
